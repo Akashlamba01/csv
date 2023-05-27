886c93f65ab99d75319aa3310273c5ca87490d93
Rajan Lamba <rk1759593@gmail.com>
2023-05-27 22:01:17 +0530

    update db

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 // XY8piF8vNDPwjAFC
 mongoose
   .connect(
-    "mongodb+srv://rk1759593:XY8piF8vNDPwjAFC@cluster0.tvdlqln.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://rk1759593:XY8piF8vNDPwjAFC@cluster0.tvdlqln.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
     console.log("connection connected!");
